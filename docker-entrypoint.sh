80941ec418ed81ff46541ce40f5bacc25edcb18d
winewei <winewei@yeah.net>
2019-07-30 10:35:25 +0800

    enable laravel config cache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 if [[ "${PARAMS}" == "nginx" ]]; then
-        chown -R www.www /var/web
-        find /var/web/www -type d -exec chmod 750 {} \;
-        find /var/web/www -not -type d -exec chmod 640 {} \;
-        find /var/web/www -name "storage" -type d -exec chmod -R 777 {} \;
+        chown -R www.www /srv
         /usr/local/openresty/nginx/sbin/nginx -g "daemon off;"
-
 fi
 
 if [[ "${PARAMS}" == "php" ]]; then
-        chown -R www.www /var/web
-        find /var/web/www -type d -exec chmod 750 {} \;
-        find /var/web/www -not -type d -exec chmod 640 {} \;
-        find /var/web/www -name "storage" -type d -exec chmod -R 777 {} \;
+        chown -R www.www /srv
+		find /srv -name "storage" -type d -exec chmod -R 777 {} \;
+		find /srv -name "bootstrap" -type d -exec chmod -R 777 {} \;
         php-fpm 
+		if [[ "${ENABLE_LARAVEL_CONFIG_CACHE}" == "true" ]]; then
+           php artisan config:cache
+		   php artisan route:cache
+		fi
 fi
 exec "$@"
