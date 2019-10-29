54c821061f8e6b82622ff908ead68be4eeaff078
winewei <winewei@yeah.net>
2019-10-29 15:06:33 +0800

    update script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-# Init npm/github token
-. /scripts/token-init.sh
 
-# Init config
-init_scripts="/scripts/init.d/*"
-for script in $init_scripts; do
-    if [ -f $script -a -x $script ]; then
-        echo "start init script: ${script}"
-        . $script
-    fi
-done
 
 # Start nginx
 if [[ "${PARAMS}" == "nginx" ]]; then
         /usr/local/openresty/nginx/sbin/nginx -g "daemon off;"
-fi
-
 # Start php
-if [[ "${PARAMS}" == "php" ]]; then
+elif [[ "${PARAMS}" == "php" ]]; then
+        # Init config
+        init_scripts="/scripts/init.d/*"
+        for script in $init_scripts; do
+            if [ -f $script -a -x $script ]; then
+                echo "start init script: ${script}"
+                . $script
+            fi
+        done
+
         php-fpm
+else
+        # Init npm/github token
+        . /scripts/token-init.sh
 fi
 
 exec "$@"
